9b8a012e9e1a3e199788904e8206f68d1f1a923a
JarRajko <48447054+JarRajko@users.noreply.github.com>
2026-02-13 16:08:14 +0100

    added prefixes and postfixes functionality

--- NickNameGenerator/Form1.Designer.cs
+++ NickNameGenerator/Form1.Designer.cs
@@ -28,24 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBoxNouns = new TextBox();
             button1 = new Button();
             checkBoxAdjective = new CheckBox();
             checkBoxNumber = new CheckBox();
             checkBoxPrefix = new CheckBox();
             checkBoxPostfix = new CheckBox();
+            labelText = new Label();
             SuspendLayout();
-            // 
-            // textBoxNouns
-            // 
-            textBoxNouns.Location = new Point(309, 101);
-            textBoxNouns.Name = "textBoxNouns";
-            textBoxNouns.Size = new Size(316, 23);
-            textBoxNouns.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(215, 101);
+            button1.Location = new Point(12, 5);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 1;
@@ -56,7 +49,7 @@
             // checkBoxAdjective
             // 
             checkBoxAdjective.AutoSize = true;
-            checkBoxAdjective.Location = new Point(215, 136);
+            checkBoxAdjective.Location = new Point(12, 40);
             checkBoxAdjective.Name = "checkBoxAdjective";
             checkBoxAdjective.Size = new Size(75, 19);
             checkBoxAdjective.TabIndex = 2;
@@ -66,7 +59,7 @@
             // checkBoxNumber
             // 
             checkBoxNumber.AutoSize = true;
-            checkBoxNumber.Location = new Point(215, 161);
+            checkBoxNumber.Location = new Point(102, 63);
             checkBoxNumber.Name = "checkBoxNumber";
             checkBoxNumber.Size = new Size(70, 19);
             checkBoxNumber.TabIndex = 3;
@@ -77,7 +70,7 @@
             // checkBoxPrefix
             // 
             checkBoxPrefix.AutoSize = true;
-            checkBoxPrefix.Location = new Point(215, 186);
+            checkBoxPrefix.Location = new Point(12, 90);
             checkBoxPrefix.Name = "checkBoxPrefix";
             checkBoxPrefix.Size = new Size(56, 19);
             checkBoxPrefix.TabIndex = 4;
@@ -87,24 +80,32 @@
             // checkBoxPostfix
             // 
             checkBoxPostfix.AutoSize = true;
-            checkBoxPostfix.Location = new Point(215, 211);
+            checkBoxPostfix.Location = new Point(12, 115);
             checkBoxPostfix.Name = "checkBoxPostfix";
             checkBoxPostfix.Size = new Size(62, 19);
             checkBoxPostfix.TabIndex = 5;
             checkBoxPostfix.Text = "Postfix";
             checkBoxPostfix.UseVisualStyleBackColor = true;
             // 
+            // labelText
+            // 
+            labelText.Location = new Point(102, 9);
+            labelText.Name = "labelText";
+            labelText.Size = new Size(316, 23);
+            labelText.TabIndex = 6;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            AutoSize = true;
+            ClientSize = new Size(562, 236);
+            Controls.Add(labelText);
             Controls.Add(checkBoxPostfix);
             Controls.Add(checkBoxPrefix);
             Controls.Add(checkBoxNumber);
             Controls.Add(checkBoxAdjective);
             Controls.Add(button1);
-            Controls.Add(textBoxNouns);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
             Text = "Nick name generator";
@@ -113,12 +114,11 @@
         }
 
         #endregion
-
-        private TextBox textBoxNouns;
         private Button button1;
         private CheckBox checkBoxAdjective;
         private CheckBox checkBoxNumber;
         private CheckBox checkBoxPrefix;
         private CheckBox checkBoxPostfix;
+        private Label labelText;
     }
 }
--- NickNameGenerator/Form1.cs
+++ NickNameGenerator/Form1.cs
@@ -5,6 +5,8 @@ namespace NickNameGenerator
 
         private string[] _nouns;
         private string[] _adjectives;
+        private string[] _prefixes;
+        private string[] _postfixes;
 
 
         public Form1()
@@ -13,6 +15,8 @@ namespace NickNameGenerator
             FileOpener fp = new FileOpener();
             _nouns = fp.getWordsFromTextFile("nouns.txt");
             _adjectives = fp.getWordsFromTextFile("adjectives.txt");
+            _prefixes = fp.getWordsFromTextFile("Prefixes.txt");
+            _postfixes = fp.getWordsFromTextFile("Postfixes.txt");
 
         }
 
@@ -21,7 +25,28 @@ namespace NickNameGenerator
             Random rand = new Random();
             string noun = _nouns[rand.Next(_nouns.Length)];
             string adjective = _nouns[rand.Next(_adjectives.Length)];
-            textBoxNouns.Text = adjective + " " + noun;
+            string prefix = _prefixes[rand.Next(_prefixes.Length)];
+            string postfix = _postfixes[rand.Next(_postfixes.Length)];
+
+            string nickname = noun;
+
+
+            if (checkBoxPrefix.Checked)
+            {
+                nickname = prefix + " " + nickname;
+            }
+
+            if (checkBoxAdjective.Checked)
+            {
+                nickname = adjective + " " + nickname;
+            }
+
+            if (checkBoxPostfix.Checked)
+            {
+                nickname = nickname + " " +  postfix;
+            }
+
+            labelText.Text = nickname;
         }
     }
 
